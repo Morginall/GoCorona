8b4cadd789996f06d8faaa23c9be6d2e1c34e2a9
Morginall <109231341+Morginall@users.noreply.github.com>
2022-07-13 21:06:49 +0300

    Update scrypt.js

--- js/scrypt.js
+++ js/scrypt.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
 	$('.header__burger').click(function (event) {
-		$('.header__burger,.header__content').toggleClass('active');
+		$('.header__burger,.header__content,.wrapper').toggleClass('active');
 		$('body').toggleClass('lock');
 	});
 });
